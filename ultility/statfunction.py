5ec9cf1d3333f444fc92fe34783342482c58815c
nhanbayern <147220729+nhanbayern@users.noreply.github.com>
2026-01-31 08:36:48 +0000

    Add and restrucre directory, uploading fixed dataset

---
+++ ultility/statfunction.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import spearmanr
+
+def compute_rank_ic(prediction):
+    if "rank_ic" not in prediction.columns:
+        prediction["rank_ic"] = np.nan
+
+    for _, df_t in prediction.groupby("ticker"):
+        if df_t["y_pred"].nunique() < 2 or df_t["y_true"].nunique() < 2:
+            continue
+
+        ic = spearmanr(
+            df_t["y_pred"].values,
+            df_t["y_true"].values
+        ).correlation
+
+        if np.isnan(ic):
+            continue
+
+        prediction.loc[df_t.index, "rank_ic"] = ic
+
+    return prediction
+
